f530ce7703be7c161b94df3e64ab600ec583f3f7
RodrigoRamosDeLima <131368723+RodrigoRamosDeLima@users.noreply.github.com>
2024-09-27 17:07:42 -0300

    Delete demo (4)/demo directory

--- demo (4)/demo/src/main/java/com/example/demo/repositories/FornecedorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FornecedorRepository extends JpaRepository<FornecedorRepository, Long> {
-    
-}
